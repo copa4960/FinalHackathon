f1783ca42bf42a9307d7667766f89c214ec94127
copa4960 <97624121+copa4960@users.noreply.github.com>
2023-03-04 18:30:45 -0700

    Add files via upload

---
+++ FinalHackathon/set.js
@@ -0,0 +1,10 @@
+/*var prev = chrome.storage.local.get('key');
+
+prev.addEventListener("click", function(){
+ 
+    var li = document.createElement("li");
+    var text = document.createTextNode(prev);
+    li.appendChild(text);
+    document.getElementById("unordered").appendChild(li);
+    
+});*/
